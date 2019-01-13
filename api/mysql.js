822b99ec439d48445013190eac62d0b84a005577
saucxs <saucxs@163.com>
2019-01-13 22:42:41 +0800

    backmanage

---
+++ api/mysql.js
@@ -0,0 +1,31 @@
+const mysql = require('mysql');
+
+const pool = mysql.createPool({
+  host: '127.0.0.1',
+  port: '3306',
+  user: 'songEagle',
+  password: '123456',
+  database: 'song_eagle',
+  connectionLimit: 10000
+})
+
+let query = function (sql, values) {
+  return new Promise((resolve, reject) => {
+    pool.getConnection(function (err, connection) {
+      if (err) {
+        return reject(err);
+      } else {
+        connection.query(sql, values, (err, rows) => {
+          connection.release();
+          if (err) {
+            return reject(err)
+          } else {
+            return resolve(rows);
+          }
+        })
+      }
+    })
+  })
+}
+
+module.exports = query;
