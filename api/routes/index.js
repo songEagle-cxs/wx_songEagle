0b306d319340dbf3a42d10e8fc9f482effbb9706
saucxs <saucxs@163.com>
2019-01-15 22:47:41 +0800

    update

--- api/routes/index.js
+++ api/routes/index.js
@@ -2,6 +2,6 @@ const router = require('koa-router')();
 const contact = require('../contact/index.js');
 
 
-router.use('/contact', contact.routes(), contact.allowedMethods());
+router.use('/blogapi', contact.routes(), contact.allowedMethods());
 
 module.exports = router;
